7cf3eeb71c74830d72476c255b7a56cfb5766c39
Ketsadaphone BOUTPANYDA <100824073+JarnKet@users.noreply.github.com>
2023-04-24 23:27:41 +0700

    Add og

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,18 +3,11 @@ import { ThemeProvider } from 'next-themes';
 import Script from 'next/script';
 import '../styles/globals.scss';
 import Head from 'next/head';
-import { metaContent } from '../constants';
-import { previewImg } from '../../public';
-
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
         <title>Code Talker | ບົດຄວາມເພື່ອໂປຣແກຣມເມີ້</title>
-        <meta name="keywords" content={metaContent.keyword.join(', ')} />
-        <meta name="description" content={metaContent.description} />
-        <meta property="og:image" content={previewImg} />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
       <ThemeProvider enableSystem={true} attribute="class">
         <Layout>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+import { metaContent } from '../constants';
+import { previewImg } from '/public';
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
@@ -8,7 +11,18 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <meta name="keywords" content={metaContent.keyword.join(', ')} />
+          <meta name="description" content={metaContent.description} />
+          <link rel="icon" href="/favicon.ico" />
+          <meta property="og:title" content="Code Talker" />
+          <meta property="og:type" content="website" />
+          <meta property="og:image" content={previewImg.src} />
+          <meta
+            property="og:url"
+            content="https://code-talker-blog.vercel.app/"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
