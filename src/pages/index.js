7781138b908cbf89134bdf5b2e680ec7cec62537
Ketsadaphone BOUTPANYDA <100824073+JarnKet@users.noreply.github.com>
2023-04-25 16:38:33 +0700

    Revert "update OpenGraph meta tag"
    This reverts commit 8c4b528ebc89933430cabb41837a60a30d3e580c.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
-import Head from 'next/head';
 import { PostCard, Categories } from '../components';
 import { getPosts } from '../services';
 import { FeaturedPost, Hero } from '../sections';
 import { ChartBarIcon } from '@heroicons/react/24/solid';
 import { useState, useEffect } from 'react';
 import { metaContent } from '../constants';
+import { previewImg } from '/public';
 
 export default function Home() {
   const [posts, setPosts] = useState([]);
@@ -23,6 +23,17 @@ export default function Home() {
         <title>Code Talker | ບົດຄວາມເພື່ອໂປຣແກຣມເມີ້</title>
         <meta name="keywords" content={metaContent.keyword.join(', ')} />
         <meta name="description" content={metaContent.description} />
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          property="og:title"
+          content="Code Talker | ບົດຄວາມເພື່ອໂປຣແກຣມເມີ້"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content={previewImg} />
+        <meta
+          property="og:url"
+          content="https://code-talker-blog.vercel.app/"
+        />
       </Head>
       <section className="border-b dark:border-b-neutral-700">
         <div className="container flex items-center justify-center w-full h-screen px-8 mx-auto lg:px-10">
