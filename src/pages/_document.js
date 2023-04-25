eb36ee0ea3b821770fa8a19f01034ecb7132bdd5
Ketsadaphone BOUTPANYDA <100824073+JarnKet@users.noreply.github.com>
2023-04-25 13:05:38 +0700

    fix document

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,7 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 import { metaContent } from '../constants';
-import previewImg from '/public/images/previewImg.png';
+
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
