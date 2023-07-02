2b95344eeb2c29778b59a9404908611e7424a79c
Ketsadaphone BOUTPANYDA <100824073+JarnKet@users.noreply.github.com>
2023-07-02 22:56:17 +0700

    Remove unused content

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,33 +1,31 @@
 const staticNavLinks = [
-  { title: 'ນັກຂຽນ', href: '/author' },
-  { title: 'ກ່ຽວກັບເຮົາ', href: '/about' },
+  { title: "ນັກຂຽນ", href: "/author" },
+  { title: "ກ່ຽວກັບເຮົາ", href: "/about" },
 ];
 
 const metaContent = {
-  keyword: ['Code Talker Blog', 'Blog', 'ບົດຄວາມ'],
+  keyword: ["Code Talker Blog", "Blog", "ບົດຄວາມ"],
   description:
-    'ບົດຄວາມ ແລະ ເລື່ອງສັ້ນ ທີ່ກ່ຽວກັບພຶດຕິກຳນິດໄສ, ທັດສະນະຄະຕິ ແລະ ວິຊາການ ເພື່ອພັດທະນາຕົນເອງໃຫ້ກາຍເປັນ Programmer ທີ່ລ້ຳຄ່າ',
+    "ບົດຄວາມ ແລະ ເລື່ອງສັ້ນ ທີ່ກ່ຽວກັບພຶດຕິກຳນິດໄສ, ທັດສະນະຄະຕິ ແລະ ວິຊາການ ເພື່ອພັດທະນາຕົນເອງໃຫ້ກາຍເປັນ Programmer ທີ່ລ້ຳຄ່າ",
 };
 
 const formLinks = {
-  interview: 'https://forms.gle/nR1DmuaktgAkuopo7',
-  registerOrSubmit: 'https://forms.gle/2CjWtgAwkbSohCzj7',
+  interview: "https://forms.gle/nR1DmuaktgAkuopo7",
+  registerOrSubmit: "https://forms.gle/2CjWtgAwkbSohCzj7",
 };
 
 const sectionDescription = {
   author:
-    'ເຂົ້າມາເປັນສ່ວນໜຶ່ງຂອງພວກເຮົາ ເພື່ອພັດທະນາຊຸມຊົນໂປຣແກຣມເມີ້ ແລະ ຄົນທີ່ຕ້ອງການພັດທະນາຕົນເອງ',
-  hero: 'ບົດຄວາມດີໆຈາກ Programmer ເພື່ອ Programmer ແລະ ຄົນທີ່ຕ້ອງການພັດທະນາຕົນເອງ',
-  heroTitle: 'ຖ້າຢາກພັດທະນາຕົນເອງ, ເວັບໄຊທ໌ນີ້ເໝາະສຳລັບທ່ານ',
+    "ເຂົ້າມາເປັນສ່ວນໜຶ່ງຂອງພວກເຮົາ ເພື່ອພັດທະນາຊຸມຊົນໂປຣແກຣມເມີ້ ແລະ ຄົນທີ່ຕ້ອງການພັດທະນາຕົນເອງ",
   about:
-    'Code Talker ເປັນເວັບບັຼອກທີ່ສ້າງຂຶ້ນມາເພື່ອຈຸດປະສົງໃນການສົ່ງເສີມການພັດທະນາທັກສະຂອງໂປຣແກຣມເມີ້ ໂດຍມີເນື້ອໃນບົດຄວາມທີ່ກ່ຽວກັບຄຸນສົມບັດພື້ນຖານຂອງໂປຣແກຣມເມີ້ ເຊິ່ງຈະເນັ້ນໜັກໄປທີ່ການພັດທະນາຕົນເອງ ບໍ່ວ່າຈະເປັນ ພຶດຕິກຳ, ນິດໄສ, ທັດສະນະຄະຕິ, ປະສົບການ ແລະ ການຂຽນໂປຣແກຣມ.',
-  category: 'ເສີມສ້າງທັກສະ ແລະ ຄວາມຮູ້ ຜ່ານບົດຄວາມໃນແຕ່ລະໝວດ ',
+    "Code Talker ເປັນເວັບບັຼອກທີ່ສ້າງຂຶ້ນມາເພື່ອຈຸດປະສົງໃນການສົ່ງເສີມການພັດທະນາທັກສະຂອງໂປຣແກຣມເມີ້ ໂດຍມີເນື້ອໃນບົດຄວາມທີ່ກ່ຽວກັບຄຸນສົມບັດພື້ນຖານຂອງໂປຣແກຣມເມີ້ ເຊິ່ງຈະເນັ້ນໜັກໄປທີ່ການພັດທະນາຕົນເອງ ບໍ່ວ່າຈະເປັນ ພຶດຕິກຳ, ນິດໄສ, ທັດສະນະຄະຕິ, ປະສົບການ ແລະ ການຂຽນໂປຣແກຣມ.",
+  category: "ເສີມສ້າງທັກສະ ແລະ ຄວາມຮູ້ ຜ່ານບົດຄວາມໃນແຕ່ລະໝວດ ",
 };
 
 const contactLinks = {
-  github: 'https://github.com/JarnKet',
+  github: "https://github.com/JarnKet",
   facebook:
-    'https://www.facebook.com/profile.php?id=100092487816483&mibextid=LQQJ4d',
+    "https://www.facebook.com/profile.php?id=100092487816483&mibextid=LQQJ4d",
 };
 
 export {
