e4592fb809019c6f69b184391cc45139b2a39a9b
Ketsadaphone BOUTPANYDA <100824073+JarnKet@users.noreply.github.com>
2023-07-02 22:56:31 +0700

    Add Head Meta Data

--- src/pages/post/index.js
+++ src/pages/post/index.js
@@ -1,6 +1,7 @@
 import { getPosts } from "../../services";
 import { Categories, PostCard } from "../../components";
 import { useState, useEffect } from "react";
+import Head from "next/head";
 
 const Post = ({ style }) => {
   const [posts, setPosts] = useState([]);
@@ -14,23 +15,33 @@ const Post = ({ style }) => {
   }, []);
 
   return (
-    <section
-      className={`${style} container flex-col items-center justify-center px-8 py-10 mx-auto mb-10 lg:flex lg:min-h-screen lg:px-10 gap-y-6 `}
-      id="postcard"
-    >
-      <h1 className="mb-4 text-4xl font-bold text-center">ບົດຄວາມລ່າສຸດ.</h1>
-      <div id="postcard" className="grid grid-cols-1 gap-x-8 lg:grid-cols-12 ">
-        <div className="col-span-1 p-8 shadow-xl lg:col-span-8 card dark:cardDark rounded-2xl">
-          {dataLoaded &&
-            posts.map((post) => <PostCard post={post.node} key={post.title} />)}
-        </div>
-        <div className="hidden lg:block lg:col-span-4">
-          <div className="relative lg:sticky top-8 ">
-            <Categories />
+    <>
+      <Head>
+        <title>Code Talker | ນັກຂຽນ</title>
+      </Head>
+      <section
+        className={`${style} container flex-col items-center justify-center px-8 py-10 mx-auto mb-10 lg:flex lg:min-h-screen lg:px-10 gap-y-6 `}
+        id="postcard"
+      >
+        <h1 className="mb-4 text-4xl font-bold text-center">ບົດຄວາມລ່າສຸດ.</h1>
+        <div
+          id="postcard"
+          className="grid grid-cols-1 gap-x-8 lg:grid-cols-12 "
+        >
+          <div className="col-span-1 p-8 shadow-xl lg:col-span-8 card dark:cardDark rounded-2xl">
+            {dataLoaded &&
+              posts.map((post) => (
+                <PostCard post={post.node} key={post.title} />
+              ))}
+          </div>
+          <div className="hidden lg:block lg:col-span-4">
+            <div className="relative lg:sticky top-8 ">
+              <Categories />
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
