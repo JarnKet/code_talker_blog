7e4dc6ae8a6cfe7e72ac1ad5701b72d6d5738d9d
Ketsadaphone BOUTPANYDA <100824073+JarnKet@users.noreply.github.com>
2023-07-09 21:39:00 +0700

    fixed lag UI

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,13 +2,13 @@ import { Header, SurveyForm, Footer } from "./";
 
 const Layout = ({ children }) => {
   return (
-    <div className="relative">
+    <>
       <Header />
       <div className=" pt-10 lg:pt-[3rem]">{children}</div>
       <Footer />
-      <div className="backgroundGrid dark:backgroundGridDark absolute inset-0 z-[-10]" />
+      <div className=" backgroundGrid dark:backgroundGridDark fixed inset-0 z-[-10]" />
       <div className="fixed inset-0 gradient" />
-    </div>
+    </>
   );
 };
 
