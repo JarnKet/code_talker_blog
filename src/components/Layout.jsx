f30a39aebb6ca8c6eb847d0ce8ca58e37c3eb4f6
JarnKet <ketsadaphone999@gmail.com>
2023-07-29 10:39:27 +0700

    remove unused styles

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -6,8 +6,6 @@ const Layout = ({ children }) => {
       <Header />
       <div className=" pt-10 lg:pt-[3rem]">{children}</div>
       <Footer />
-      <div className="hidden lg:flex backgroundGrid dark:backgroundGridDark fixed inset-0 z-[-10]" />
-      <div className="fixed inset-0 gradient" />
     </>
   );
 };
