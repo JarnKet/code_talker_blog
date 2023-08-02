497d7195005b4d7cbe92e986abade2dbccdecc1c
JarnKet <ketsadaphone999@gmail.com>
2023-08-02 09:50:25 +0700

    remove unused component

--- src/components/index.js
+++ src/components/index.js
@@ -7,7 +7,6 @@ export { default as Author } from "./Author";
 export { default as PostDetail } from "./PostDetail";
 export { default as Comments } from "./Comments";
 export { default as CommentsForm } from "./CommentsForm";
-export { default as FeaturedPostCard } from "./FeaturedPostCard";
 export { default as Loader } from "./Loader";
 export { default as Footer } from "./Footer";
 export { default as PostReference } from "./PostReference";
